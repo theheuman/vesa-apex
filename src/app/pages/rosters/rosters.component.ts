efe3a9068d007d4c34ca6cfa87c223512054e61e
TheHeuman <jacob.heuman@gmail.com>
2024-09-04 14:57:48 -0400

    show teams on roster page

--- src/app/pages/rosters/rosters.component.ts
+++ src/app/pages/rosters/rosters.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Team } from '../../services/rosters/classes';
+import { RostersService } from '../../services/rosters/rosters.service';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-rosters',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './rosters.component.html',
   styleUrl: './rosters.component.scss',
 })
-export class RostersComponent {}
+export class RostersComponent {
+  loading = true;
+  leagueTeams: Team<3, 3>[];
+  vcsTeams: Team<3, 5>[];
+
+  constructor(private rosterService: RostersService) {
+    this.leagueTeams = [];
+    this.vcsTeams = [];
+    this.getTeams().then(() => {
+      this.loading = false;
+    });
+  }
+
+  async getTeams() {
+    this.leagueTeams = await this.rosterService.getTeams(3, 3);
+    this.vcsTeams = await this.rosterService.getTeams(3, 5);
+  }
+}
